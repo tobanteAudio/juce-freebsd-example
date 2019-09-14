b1955a32a2fee1e3db4334e080c80d85f8d6518a
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-09-14 23:08:43 +0000

    Use juce code in console_app example

--- console_app/Source/main.cpp
+++ console_app/Source/main.cpp
@@ -1,9 +1,22 @@
 #include "JuceHeader.h"
 
+String readFile (const File& fileToRead)
+{
+    if (! fileToRead.existsAsFile()) // [1]
+        return "";
+
+    auto fileText = fileToRead.loadFileAsString();
+    return fileText;
+}
+
+
 int main(int, char **)
 {
+  auto path = File("Source/main.cpp");
+
   std::cout << "TEST\n";
-  File path ("main.cpp");
+  std::cout << readFile(path) << '\n';
+
   DBG("TEST");
 
   return 0;
